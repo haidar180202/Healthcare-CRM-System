9e2ebdd8b2038a4d597c7000b2130ab138f69f2c
haidar180202 <gshaidar6@gmail.com>
2026-01-23 22:28:46 +0700

    chore(Healthcare-CRM-System): update docker config

--- frontend/src/apollo.js
+++ frontend/src/apollo.js
@@ -1,17 +1,15 @@
 import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
-import { WebSocketLink } from '@apollo/client/link/ws';
+import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
+import { createClient } from 'graphql-ws';
 import { getMainDefinition } from '@apollo/client/utilities';
 
 const httpLink = new HttpLink({
-  uri: 'http://localhost:3000/graphql',
+  uri: 'http://localhost:4000/graphql',
 });
 
-const wsLink = new WebSocketLink({
-  uri: 'ws://localhost:3000/graphql',
-  options: {
-    reconnect: true,
-  },
-});
+const wsLink = new GraphQLWsLink(createClient({
+  url: 'ws://localhost:4000/graphql',
+}));
 
 const splitLink = split(
   ({ query }) => {
